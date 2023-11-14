ee0956d1c82578e2129c0ca2691fcf9072f4b894
PRANAB NANDY <34576104+PranabNandy@users.noreply.github.com>
2023-11-14 23:29:44 +0530

    Update pcd_device_setup.c

--- 5.Linux-Device-Driver/2.1.Char_Platform_Device_Driver/pcd_device_setup.c
+++ 5.Linux-Device-Driver/2.1.Char_Platform_Device_Driver/pcd_device_setup.c
@@ -1,99 +1,108 @@
-/*
- * @brief: Create 2 platform devices and initialize them with required information
- *         Register platform devices with the Linux kernel
- * @author: NghiaPham
- * @date: 2020/09/30
- * @version: v0.1
- * 
- */
-#include <linux/module.h>
-#include <linux/platform_device.h>
+#include<linux/module.h>
+#include<linux/platform_device.h>
+
 #include "platform.h"
 
 #undef pr_fmt
-#define pr_fmt(fmt) "[%s]: " fmt, __func__
+#define pr_fmt(fmt) "%s : " fmt,__func__
 
-/* Callback to free the device after all references have gone away */
-void pcdev_release(struct device *dev) {
-    // TODO: 
-    pr_info("Device released\n");
+void pcdev_release(struct device *dev)
+{
+	pr_info("Device released \n");
 }
 
-/* Create two platform data */
-struct pcdev_platform_data pcdev_pdata[] = {
-    [0] = {.size = 512, .permission = RDWR, .serial_number = "PCDEV_1"},
-    [1] = {.size = 512, .permission = RDWR, .serial_number = "PCDEV_2"},
-    [2] = {.size = 1024, .permission = RDONLY, .serial_number = "PCDEV_3"},
-    [3] = {.size = 2048, .permission = WRONLY, .serial_number = "PCDEV_4"}
+
+/*1. create n platform data */
+struct pcdev_platform_data  pcdev_pdata[] = {
+	[0] = {.size = 512, .perm = RDWR,   .serial_number = "PCDEVABC1111"},
+	[1] = {.size = 1024,.perm = RDWR,   .serial_number = "PCDEVXYZ2222"},
+	[2] = {.size = 128, .perm = RDONLY, .serial_number = "PCDEVXYZ3333"},
+	[3] = {.size = 32,  .perm = WRONLY, .serial_number = "PCDEVXYZ4444"}
 };
 
-/* Create two platform device */
+/*2. create n platform devices */ 
+
 struct platform_device platform_pcdev_1 = {
-    .name = "pcdev-Ax",
-    .id = 0,
-    .dev = {
-        .platform_data = &pcdev_pdata[0],
-        .release = pcdev_release
-    }
+	.name = "pcdev-A1x",
+	.id = 0,
+	.dev = {
+		.platform_data = &pcdev_pdata[0],
+		.release = pcdev_release
+	}
 };
+
 
 struct platform_device platform_pcdev_2 = {
-    .name = "pcdev-Bx",
-    .id = 1,
-    .dev = {
-        .platform_data = &pcdev_pdata[1],
-        .release = pcdev_release
-    }
+	.name = "pcdev-B1x",
+	.id = 1,
+	.dev = {
+		.platform_data = &pcdev_pdata[1],
+		.release = pcdev_release
+	}
 };
+
 
 struct platform_device platform_pcdev_3 = {
-    .name = "pcdev-Cx",
-    .id = 2,
-    .dev = {
-        .platform_data = &pcdev_pdata[2],
-        .release = pcdev_release
-    }
+	.name = "pcdev-C1x",
+	.id = 2,
+	.dev = {
+		.platform_data = &pcdev_pdata[2],
+		.release = pcdev_release
+	}
 };
+
 
 struct platform_device platform_pcdev_4 = {
-    .name = "pcdev-Dx",
-    .id = 3,
-    .dev = {
-        .platform_data = &pcdev_pdata[3],
-        .release = pcdev_release
-    }
+	.name = "pcdev-D1x",
+	.id = 3,
+	.dev = {
+		.platform_data = &pcdev_pdata[3],
+		.release = pcdev_release
+	}
 };
 
-struct platform_device *platform_pcdevs[] = {
-    &platform_pcdev_1,
-    &platform_pcdev_2,
-    &platform_pcdev_3,
-    &platform_pcdev_4
+
+struct platform_device *platform_pcdevs[] = 
+{
+	&platform_pcdev_1,
+	&platform_pcdev_2,
+	&platform_pcdev_3,
+	&platform_pcdev_4
 };
 
-static int __init pcdev_platform_init(void) {
-    /* Register platform-level device */
-    platform_add_devices(platform_pcdevs, ARRAY_SIZE(platform_pcdevs));
+static int __init pcdev_platform_init(void)
+{
+	/* register n platform devices */
 
-    pr_info("Platform device setup module loaded\n");
+	//platform_device_register(&platform_pcdev_1);
+	//platform_device_register(&platform_pcdev_2);
+	
+	platform_add_devices(platform_pcdevs,ARRAY_SIZE(platform_pcdevs) );
 
-    return 0;
+	pr_info("Device setup module loaded \n");
+
+	return 0;
 }
 
-static void __exit pcdev_platform_exit(void) {
-    /* Unregister platform-level device */
-    platform_device_unregister(&platform_pcdev_1);
-    platform_device_unregister(&platform_pcdev_2);
-    platform_device_unregister(&platform_pcdev_3);
-    platform_device_unregister(&platform_pcdev_4);
 
-    pr_info("Platform device setup module unloaded\n");
+static void __exit pcdev_platform_exit(void)
+{
+
+	platform_device_unregister(&platform_pcdev_1);
+	platform_device_unregister(&platform_pcdev_2);
+	platform_device_unregister(&platform_pcdev_3);
+	platform_device_unregister(&platform_pcdev_4);
+	pr_info("Device setup module unloaded \n");
+
+
 }
 
 module_init(pcdev_platform_init);
 module_exit(pcdev_platform_exit);
 
+
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("NghiaPham");
-MODULE_DESCRIPTION("Register platform devices with the Linux kernel");
-MODULE_INFO(board,"Beaglebone Black rev.c");
+MODULE_DESCRIPTION("Module which registers n platform devices ");
+MODULE_AUTHOR("Kiran Nayak");
+
+
